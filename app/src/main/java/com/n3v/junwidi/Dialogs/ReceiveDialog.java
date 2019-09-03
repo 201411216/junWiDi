c8507225eb6688e7cecb08b516c5780259d5eb31
jun.n3v <jun.nevahn@gmail.com>
2019-09-03 15:54:03 +0900

    ~dialog UI/Class

--- app/src/main/java/com/n3v/junwidi/Dialogs/ReceiveDialog.java
+++ app/src/main/java/com/n3v/junwidi/Dialogs/ReceiveDialog.java
@@ -1,12 +1,47 @@
 package com.n3v.junwidi.Dialogs;
 
-import com.n3v.junwidi.Listener.MyDialogListener;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class ReceiveDialog {
+import com.n3v.junwidi.Listener.MyDialogListener;
+import com.n3v.junwidi.R;
+
+public class ReceiveDialog extends Dialog {
 
     MyDialogListener myDialogListener;
-    int progress;
+    int progress = 0;
+
+    Context mContext;
+
+    String fileName;
+
+    private Button okButton;
+    private Button cancelButton;
+    private ProgressBar progressBar;
+    private TextView percentage;
+    private TextView videoTitle;
 
 
+    public ReceiveDialog(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    public ReceiveDialog(Context context, String fileName) {
+        super(context);
+        mContext = context;
+        this.fileName = fileName;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.receive_dialog);
+        okButton = findViewById(R.id.rcv_dialog_btn_ok);
+    }
 
 }
