7e92c756036d46f263bfa31f39afd862b367dfdb
201411164 <37547603+201411164@users.noreply.github.com>
2019-09-07 19:49:54 +0900

    Loading Screen Updated
    with Splash_background java and xml

---
+++ app/src/main/java/com/n3v/junwidi/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.n3v.junwidi;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import static java.lang.Thread.sleep;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try{
+            sleep(800);
+        }
+        catch(Exception e)
+        {
+
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
+
